5cbf7ddceab03249ae1ecef203837a0e7121e511
Christopher Køltzow <christopherk88@gmail.com>
2022-10-23 21:41:37 +0200

    add logo icon for mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import { IconMap, IconSearch } from "../icons";
 import LoginButton from "../loginbutton";
-import { NorwegianLogo } from "../logo";
+import { NorwegianLogo, LogoIcon } from "../logo";
 
 const Header = (): JSX.Element => {
 
     return (
         <header className="border-b px-8 flex justify-center">
             <nav className="max-w-[1440px] w-full flex h-20 items-center justify-between">
-                <div className="flex gap-8 items-center">
+                <div className="flex gap-4 md:gap-8 items-center">
                     <Link href="/" passHref>
-                        <a><NorwegianLogo /></a>
+                        <a><span className="hidden md:inline"><NorwegianLogo /></span><span className="md:hidden"><LogoIcon /></span></a>
                     </Link>
                     <div className="flex gap-2 items-center">
                         <button className="inline-flex gap-2 py-2 px-2 md:px-4 rounded-full">
@@ -18,7 +18,7 @@ const Header = (): JSX.Element => {
                             <span className="hidden md:inline">Søk</span>
                         </button>
                         <Link href="/prosjekter" passHref>
-                            <a className="inline-flex py-2 px-2 md:px-4 gap-2 rounded-full">
+                            <a className="inline-flex py-2 px-2 md:px-4 md:gap-2 rounded-full">
                                 <span><IconMap /></span>
                                 <span className="hidden md:inline">Vis boliger på kart</span>
                             </a>
