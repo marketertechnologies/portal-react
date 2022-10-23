7fbc940a26720fb76bce4bd7cd3c2954a81ceed2
Christopher Køltzow <christopherk88@gmail.com>
2022-10-23 21:42:02 +0200

    update search recommendations style

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -1,41 +1,39 @@
 import Link from "next/link";
+import React, { ReactNode } from "react";
+
+const Pill = ({ href, children }: {
+    href: string;
+    children: ReactNode
+}) => (
+    <Link href={href} passHref>
+        <a className="inline-flex px-3 py-1 md:px-4 md:py-2 rounded-full bg-secondary hover:bg-[#ddd] hover:border-black">{children}</a>
+    </Link>
+);
 
 const SearchBar = () => {
     return (
         <div className="px-8 flex justify-center mb-8">
-            <div className="max-w-[1440px] w-full gap-4 flex flex-col">
+            <div className="max-w-[1440px] w-full gap-8 flex flex-col">
                 {/* <p>Søk etter ønsket bosted</p> */}
-                <input className="font-primary border-b text-3xl md:text-4xl py-4 w-full" placeholder="Finn din nye bolig" type="search" />
+                <input className="font-primary border-b text-3xl md:text-4xl py-4 w-full rounded-none" placeholder="Finn din nye bolig" type="search" />
                 <ul className="flex gap-2 flex-wrap">
                     <li>
-                        <Link href="/prosjekter?city=oslo" passHref>
-                            <a className="inline-flex px-4 py-2 border rounded-full hover:border-black">Oslo</a>
-                        </Link>
+                        <Pill href="/prosjekter?city=oslo">Oslo</Pill>
                     </li>
                     <li>
-                        <Link href="/prosjekter?city=bergen" passHref>
-                            <a className="inline-flex px-4 py-2 border rounded-full hover:border-black">Bergen</a>
-                        </Link>
+                        <Pill href="/prosjekter?city=bergen">Bergen</Pill>
                     </li>
                     <li>
-                        <Link href="/prosjekter?city=stavanger" passHref>
-                            <a className="inline-flex px-4 py-2 border rounded-full hover:border-black">Stavanger</a>
-                        </Link>
+                        <Pill href="/prosjekter?city=stavanger">Stavanger</Pill>
                     </li>
                     <li>
-                        <Link href="/prosjekter?city=trondheim" passHref>
-                            <a className="inline-flex px-4 py-2 border rounded-full hover:border-black">Trondheim</a>
-                        </Link>
+                        <Pill href="/prosjekter?city=trondheim">Trondheim</Pill>
                     </li>
                     <li>
-                        <Link href="/prosjekter?city=fredrikstad" passHref>
-                            <a className="inline-flex px-4 py-2 border rounded-full hover:border-black">Fredrikstad</a>
-                        </Link>
+                        <Pill href="/prosjekter?city=fredrikstad">Fredrikstad</Pill>
                     </li>
                     <li>
-                        <Link href="/prosjekter?city=nearby" passHref>
-                            <a className="whitespace-nowrap inline-flex px-4 py-2 border rounded-full hover:border-black">I nærheten</a>
-                        </Link>
+                        <Pill href="/prosjekter?city=nearby">I nærheten</Pill>
                     </li>
                 </ul>
             </div>
