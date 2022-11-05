4cc646177a72e131de100be750d71e3d52eb8be1
Christopher Køltzow <christopherk88@gmail.com>
2022-11-05 22:29:47 +0100

    make images priority

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -15,7 +15,7 @@ const Gallery = ({
     return (
         <>
             <div className="pb-[56%] lg:pb-[40%] bg-black w-full relative" onClick={openGallery}>
-                {images && images[0] && <Image className="object-cover" alt="" layout="fill" src={images[0].url} />}
+                {images && images[0] && <Image priority className="object-cover" alt="" layout="fill" src={images[0].url} />}
                 <button className="bg-white text-black px-4 py-2 rounded-full absolute bottom-4 right-4 flex gap-2"><IconArrowOutSimple /> {images.length} bilder</button>
             </div>
             <GalleryModal images={images} />
